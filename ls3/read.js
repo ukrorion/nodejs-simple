531014da0adf89dfe6dc7683681fb0039567ea32
Alexander Ostach <ukrorion@gmail.com>
2014-09-10 01:13:28 +0300

    Implemented sync reading fro file

--- ls3/read.js
+++ ls3/read.js
@@ -1,6 +1,11 @@
 var fs = require("fs");
-console.log("Start working");
+console.log("Start async reading");
 fs.readFile("./ls1/sample.txt", function (error, data) {
   console.log("Data from file: "+ data);
 });
-console.log("Stop working");
+console.log("Stop async reading");
+
+console.log("Start sync reading");
+var data = fs.readFileSync("./ls1/sample.txt");
+console.log("Data from file: " + data);
+console.log("Stop sync reading");
