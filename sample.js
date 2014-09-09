bf6d438379cf9571ba57fe50d6ada4568a45d4f8
Alexander Ostach <ukrorion@gmail.com>
2014-09-10 00:45:30 +0300

    Small changes

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 console.log("Start");
 fs.readFile("sample.txt", function(error,data){
-  console.log("Content of file" + data);
+  console.log("Content of file " + data);
 });
 console.log("Stop");
