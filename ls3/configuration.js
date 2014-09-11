a229024f331943c702c39662ba67c618b3d4c770
Alexander Ostach <ukrorion@gmail.com>
2014-09-12 00:40:01 +0300

    Added parsing of configuration into object

--- ls3/configuration.js
+++ ls3/configuration.js
@@ -2,3 +2,6 @@ var fs = require('fs');
 console.log('Start working');
 var content = fs.readFileSync('./files/config.json');
 console.log('Content '+ content);
+var config = JSON.parse(content);
+console.log('Config: ',config);
+console.log('Username ', config.username);
