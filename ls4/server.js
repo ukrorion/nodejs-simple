2200dce60006faa48a65c0154d21a29bd979a9db
Alexander Ostach <ukrorion@gmail.com>
2014-11-06 02:15:52 +0200

    Added static files and simple routing

--- ls4/server.js
+++ ls4/server.js
@@ -1,12 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 console.log("Starting...");
 var host = "127.0.0.1";
 var port = "3000";
 var server = http.createServer(function (request, response) {
 	console.log("Received request: " + request.url);
-	response.writeHead(200, {"Content-type":"text/plain"});
-	// response.write("Text for user");
-	response.end("Text for user"); //Set a body message to an end method instead a write method.
+  fs.readFile("public" + request.url, function (error, data) {
+    console.log(error);
+    if(error){
+      fs.readFile("public/404.html", function (error, data) {
+        response.writeHead(404, {"Content-type":"text/html"});
+        response.end(data);
+      });
+    } else {
+      response.writeHead(200, {"Content-type":"text/html"});
+      response.end(data);
+    }
+  });
 });
 server.listen(port, host, function() {
 	console.log("Server listening " + host + " on " + port)
