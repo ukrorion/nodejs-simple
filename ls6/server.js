364f7b5d4f1965ce2387c331e528087eee4dc2a4
Alexander Ostach <ukrorion@gmail.com>
2014-11-07 01:55:02 +0200

    Added some routes to app

--- ls6/server.js
+++ ls6/server.js
@@ -1,11 +1,21 @@
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync("config/server.json"));
 var express = require('express');
+var router = express.Router();
+var logger = require('morgan');
 
 var app = express();
 
-app.get("/", function (request, response) {
+router.use(logger());
+app.get("/", function (request, response, next) {
   response.send("This is the index page");
+});
+app.use(express.static(__dirname + '/public'));
+app.get("/news/:id",  function (req, res) {
+  res.send("News #" + req.params.id);
+});
+app.get(function(req, res){
+  res.send("Page not found!");
 });
 
 app.listen(config.port, config.host);
