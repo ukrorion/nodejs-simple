ecb08468768bf16fa162a3bdd80ace3f05f3f9b8
Alexander Ostach <ukrorion@gmail.com>
2014-11-11 01:24:05 +0200

    Wrapped request into an async function

--- ls7/request.js
+++ ls7/request.js
@@ -3,26 +3,34 @@
  */
 
 var https = require("https");
-var username = 'ukrorion';
 
-var options = {
-  host: 'api.github.com',
-  path: '/users/' + username + '/repos',
-  method: 'GET',
-  headers: {'User-Agent' : 'Mozilla/5.0'}
-};
+function getRepos(username, callback) {
+  var options = {
+    host: 'api.github.com',
+    path: '/users/' + username + '/repos',
+    method: 'GET',
+    headers: {'User-Agent': 'Mozilla/5.0'}
+  };
 
-var request = https.request(options, function(response){
-  var body = '';
-  response.on("data", function (chunk) {
-    body += chunk.toString('utf8');
-  });
-  response.on("end", function(){
-    var json_body = JSON.parse(body);
-    json_body.forEach(function(repo){
-      console.log("Name: " + repo.name + " Owner: " + repo.owner.login);
+  var request = https.request(options, function (response) {
+    var body = '';
+    response.on("data", function (chunk) {
+      body += chunk.toString('utf8');
     });
-    console.log("Repos count: " + json_body.length);
+    response.on("end", function () {
+      var repos = [];
+      var json_body = JSON.parse(body);
+      json_body.forEach(function (repo) {
+        repos.push(repo);
+      });
+      callback(repos);
+    });
+  });
+  request.end();
+}
+
+getRepos("ukrorion", function(repos){
+  repos.forEach(function(repo){
+    console.log("Repo name: " + repo.name + " owner: " + repo.owner.login);
   });
 });
-request.end();
