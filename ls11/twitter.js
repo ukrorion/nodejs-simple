b0cc5e0023c477bbd79a1673e0c151170e853134
Alexander Ostach <ukrorion@gmail.com>
2014-11-19 19:05:31 +0200

    Added oauth instead basic auth & package.jsno file

--- ls11/twitter.js
+++ ls11/twitter.js
@@ -1,20 +1,26 @@
-var https = require('https');
-var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('./config/twitter.json'));
+var OAuth = require('oauth').OAuth;
 
-var request = https.request(config.options, function(response){
-  var body = '';
-  response.on("data", function (chunk) {
-    //var tweet = JSON.parse(chunk);
-    console.log("Tweet: " + chunk);
-    //body += chunk.toString();
-  });
-  response.on("end", function(){
-    console.log("Disconnected from twitter");
-  });
-});
-request.end();
-
-request.on('error', function(error) {
-  console.error(error);
-});
+var connection = new OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  'wfYzSL9C1BOaf5VlNm1lw',
+  '3oe4v2CyFO1Nd0GhdzX8Ww2nWpCxBVLh1CVn0JNkuEo',
+  '1.0A',
+  null,
+  'HMAC-SHA1'
+);
+console.log(connection);
+connection.get(
+  'https://stream.twitter.com/1.1/statuses/filter.json?track=twitter',
+  '271359180-fmh4JpOdTC0K2nF0QlB2GwyI6q5Cr3Q3WDA8jd41',
+  'r0TCSqMyZDezNjv2JKvunDXPy7XwryjFeOv2gG3qsVuNM',
+  function (e, data, res){
+    if(e) {
+      console.error(e);
+    } else {
+      var s_data = JSON.parse(data);
+      console.log(s_data);
+    }
+    done();
+  }
+);
