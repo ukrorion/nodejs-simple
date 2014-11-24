34ad3d8f351199f113cb61c17461f9ab328e6e70
Alexander Ostach <ukrorion@gmail.com>
2014-11-24 16:23:47 +0200

    Checking fro open connection

--- ls11/tweets.js
+++ ls11/tweets.js
@@ -6,6 +6,7 @@ var mongo = require('mongodb');
 var host = "127.0.0.1";
 var port = "27017";
 
+// Private methods
 var connectToCollection = function(client){
   return client.db('nodejs-simple').collection("tweets");
 };
@@ -15,50 +16,34 @@ Tweet = function() {
   this.client = new mongo.MongoClient(this.server);
 };
 
-Tweet.prototype.getAll =  function(callback){
-  this.client.open(function(error, client){
-    if(error){
-      console.log("Not connected because of error: " + error);
-    } else {
-      var tweets = connectToCollection(client);
-
-      tweets.find().toArray(function (err, result) {
-        if(err) {
-          console.log("Find error: " + err);
-          client.close();
-          callback(false);
-        } else {
-          client.close();
-          callback(result);
-        }
-      });
-    }
-  });
+Tweet.prototype.openConnection = function(callback){
+  if (this.client._db._state == '‌disconnected'){
+    this.client.open(function(error, client) {
+      if (error) {
+        console.log("Not connected because of error: " + error);
+      } else {
+        callback(client);
+      }
+    });
+  } else {
+    callback(this.client);
+  }
 };
 
 Tweet.prototype.insert = function(collection, callback){
-  if (this.client._db._state == 'connecting')
-    this.client.close();
-  this.client.open(function(error, client){
-    if(error){
-      console.log("Not connected because of error: " + error);
-    } else {
-      var tweets = connectToCollection(client);
-      console.log(tweets);
-      tweets.insert(collection, function(err, result){
-        if(err){
+  this.openConnection(function(client){
+    var tweets = connectToCollection(client);
+    tweets.insert(collection, function (err, result) {
+        if (err) {
           console.log("Insert error " + err);
-          client.close();
           if (typeof callback == "function")
             callback(false);
         } else {
-          client.close();
           if (typeof callback == "function")
             callback(result);
         }
-      });
-    }
-    client.close();
+      client.close();
+    });
   });
 };
 
